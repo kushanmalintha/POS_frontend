f2293ec8c15cd8c4bf67507c1135579c5db6d355
kushan malintha <e20243@eng.pdn.ac.lk>
2026-01-03 18:06:51 +0530

    initial setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LoginForm from "./components/auth/LoginForm";
+import Dashboard from "./pages/Dashboard";
+
+function App() {
+  return (
+    <div></div>
+  );
+}
+
+export default App;
